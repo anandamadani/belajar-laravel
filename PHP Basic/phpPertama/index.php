a537151d64c250bdfe32dc2686c002c7afd8c0c4
anandamadani <anandamadani8@gmail.com>
2019-01-07 10:13:32 +0700

    first commit

---
+++ PHP Basic/phpPertama/index.php
@@ -0,0 +1,69 @@
+<?php
+
+
+//phpPertama
+//echo 'Hello World';
+/*$siswa = 'Nanda';
+
+echo $siswa;*/
+
+//variabel
+/*$jumlahSiswa = 20;
+
+$jumlahSiswa = 100;
+
+echo 'Ada 20 siswa didalam kelas' . $jumlahSiswa . 'siswa didalam kelas';*/
+
+//pengenalanTipeData
+
+//tipeData String
+
+/*$name = "Nanda";
+
+$message = $name . 'Nan "learning php".';
+
+echo $message;
+
+echo ;*/
+
+//tipeData integerAndFloat
+
+/*$myMoney = 1e9;
+
+echo $myMoney;*/
+
+//tipeData Boolean
+
+/*$isInteger = is_int('nan');
+
+if ($isInteger){
+    echo 'your is integer';
+}
+
+var_dump(is_int(1));*/
+
+//tipeData pengenalanArray
+
+/*$siswa = ['nanda', 'madani', 'nor'];
+
+$siswa[] = 'anan';
+//var_dump($siswa);
+echo $siswa[3];*/
+
+//tipeData lebihDalamTentangArray
+
+/*$n = ['edo', 'edi', 'edd'];
+
+$n[] = 'haha';
+
+echo $n[2];*/
+
+$people = [
+    'an' => 2,
+    'nan' => 3,
+    'da' => 4,
+];
+
+var_dump($people);
+
+?>
